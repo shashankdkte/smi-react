9340d5b4d9a15f5fdb9643a45d123760418a4980
Shashank Dhakate <shashankdkte@gmail.com>
2023-10-01 06:50:25 +0530

    Adding Styling

---
+++ book-application/src/Book.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Book() {
+  return (
+    <div className='book'>
+      <img src="https://d2g9wbak88g7ch.cloudfront.net/productimages/images200/700/9781974738700.jpg" alt="" className='book--img' />
+      <h3 className='book--name'>One Piece</h3>
+      <p className='author--name'>Monkey D Luffy</p>
+    </div>
+  )
+}
+
+export default Book
---
+++ book-application/src/BookList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Book from './Book'
+
+function BookList() {
+  return (
+    <div className='book--list'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </div>
+  )
+}
+
+export default BookList
