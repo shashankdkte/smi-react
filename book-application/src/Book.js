8144071a8879658cd5e7c0d049e31f8b8d69f1bd
Shashank Dhakate <shashankdkte@gmail.com>
2023-10-01 07:30:20 +0530

    Adding Data to Application

--- book-application/src/Book.js
+++ book-application/src/Book.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Book() {
+function Book({author,image,title}) {
   return (
     <div className='book'>
-      <img src="https://d2g9wbak88g7ch.cloudfront.net/productimages/images200/700/9781974738700.jpg" alt="" className='book--img' />
-      <h3 className='book--name'>One Piece</h3>
-      <p className='author--name'>Monkey D Luffy</p>
+      <img src={image}alt={title} className='book--img' />
+      <h3 className='book--name'>{title}</h3>
+      <p className='author--name'>{author}</p>
     </div>
   )
 }
--- book-application/src/BookList.js
+++ book-application/src/BookList.js
@@ -1,13 +1,88 @@
 import React from 'react'
 import Book from './Book'
 
+const books = [
+  {
+    id: 1,
+    image: "https://avt.mkklcdnv6temp.com/3/u/1-1583463814.jpg",
+    title: "One Piece",
+    author:"Oda Eiichiro"
+  },
+    {
+    id: 2,
+    image: "https://avt.mkklcdnv6temp.com/30/a/17-1583496340.jpg",
+    title: "Solo Levelling",
+    author:"Sung-Lag Jang"
+  },
+      {
+    id: 3,
+    image: "https://avt.mkklcdnv6temp.com/20/b/16-1583494192.jpg",
+    title: "Martial Peak",
+    author:"Momo"
+  },
+        {
+    id: 4,
+    image: "https://avt.mkklcdnv6temp.com/13/f/16-1583493949.jpg",
+    title: "Apothesis",
+    author:"Ranzai Studio"
+  },
+          {
+    id: 5,
+    image: "https://avt.mkklcdnv6temp.com/48/q/17-1583497020.jpg",
+    title: "The Beginning After The End",
+    author:"Turtle Me"
+  },
+            {
+    id: 6,
+    image: "https://avt.mkklcdnv6temp.com/30/e/13-1583488820.jpg",
+    title: "Kimetsu No Yaiba",
+    author:"Gotouge Koyoharu"
+  },
+              {
+    id: 7,
+    image: "https://avt.mkklcdnv6temp.com/19/y/2-1583466482.jpg",
+    title: "Tensei Shitara Sime Datta Ken",
+    author:"Kawakami Taiki"
+  },
+                {
+    id: 8,
+    image: "https://avt.mkklcdnv6temp.com/7/r/1-1583463967.jpg",
+    title: "Mushoku Tensei",
+    author:"Rifujin Na Managonte"
+  },
+                  {
+    id: 9,
+    image: "https://avt.mkklcdnv6temp.com/23/e/1-1583464609.jpg",
+    title: "My Hero Academia",
+    author:"Horikoshi Kouhei"
+  },
+                    {
+    id: 10,
+    image: "https://avt.mkklcdnv6temp.com/19/x/1-1583464480.jpg",
+    title: "Tower of God",
+    author:"Siu"
+  },
+                      {
+    id: 11,
+    image: "https://avt.mkklcdnv6temp.com/24/p/1-1583464662.jpg",
+    title: "Kaguya Sama Love is War",
+    author:"Akasaka Aka"
+  },
+                        {
+    id: 12,
+    image: "https://avt.mkklcdnv6temp.com/50/o/15-1583493481.jpg",
+    title: "Tokyo Manji",
+    author:"Wakui Ken"
+  }
+  
+]
+
 function BookList() {
   return (
     <div className='book--list'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        return <Book id={book.id} title={book.title} image={book.image} author={book.author} />
+      })}
     </div>
   )
 }
